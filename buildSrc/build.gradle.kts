c7772fc9c85a1ab55afcc627997831110b7b0c44
Adrien Poupard <1469248+apoupard@users.noreply.github.com>
2024-04-24 17:33:14 +0200

    fix(Dockerfile): update image source to use 'komune-io' instead of 'ghcr.io/komune-io' for chaincode images feat(Makefile): modify push target to tag and push images to the correct Docker repository The changes were made to update the image source in the Dockerfile to use 'komune-io' instead of 'ghcr.io/komune-io' for chaincode images. Additionally, the Makefile's push target was modified to correctly tag and push images to the appropriate Docker repository.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
 	`kotlin-dsl`
 }
 
+repositories {
+	mavenCentral()
+	maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") }
+}
+
 dependencies {
 	implementation("io.komune.fixers.gradle:dependencies:0.18.0-SNAPSHOT")
 }
+
--- docker.mk
+++ docker.mk
@@ -1,31 +1,35 @@
 VERSION = $(shell cat VERSION)
-DOCKER_REPOSITORY = ghcr.io/
 
-CHAINCODE_APP_NAME	   	 	:= ghcr.io/komune-io/c2-chaincode-api-gateway
+CHAINCODE_APP_NAME	   	 	:= c2-chaincode-api-gateway
 CHAINCODE_APP_IMG	    	:= ${CHAINCODE_APP_NAME}:${VERSION}
 CHAINCODE_APP_PACKAGE	   	:= :c2-chaincode:chaincode-api:chaincode-api-gateway:bootBuildImage
 
 .PHONY: lint build test publish promote
 
 lint:
-	make lint -e DOCKER_REPOSITORY=ghcr.io/ -C c2-chaincode -e VERSION=$(VERSION)
-	make lint -e DOCKER_REPOSITORY=ghcr.io/ -C c2-sandbox -e VERSION=$(VERSION)
+	make lint -C c2-chaincode -e VERSION=$(VERSION)
+	make lint -C c2-sandbox -e VERSION=$(VERSION)
 
 build: docker-chaincode-api-gateway-build
-	make build -e DOCKER_REPOSITORY=ghcr.io/ -C c2-chaincode -e VERSION=$(VERSION)
-	make build -e DOCKER_REPOSITORY=ghcr.io/ -C c2-sandbox -e VERSION=$(VERSION)
+	#make build -C c2-chaincode -e VERSION=$(VERSION)
+	make build -C c2-sandbox -e VERSION=$(VERSION)
 
 publish: docker-chaincode-api-gateway-publish
-	make publish -e DOCKER_REPOSITORY=ghcr.io/ -C c2-chaincode -e VERSION=$(VERSION)
-	make publish -e DOCKER_REPOSITORY=ghcr.io/ -C c2-sandbox -e VERSION=$(VERSION)
+	#make publish -e DOCKER_REPOSITORY=ghcr.io/komune-io/ -C c2-chaincode -e VERSION=$(VERSION)
+	make publish -e DOCKER_REPOSITORY=ghcr.io/komune-io/ -C c2-sandbox -e VERSION=$(VERSION)
 
-promote:
-	make publish -e DOCKER_REPOSITORY=ghcr.io/ -C c2-chaincode -e VERSION=$(VERSION)
-	make publish -e DOCKER_REPOSITORY=ghcr.io/ -C c2-sandbox -e VERSION=$(VERSION)
+promote: docker-chaincode-api-gateway-promote
+	#make promote -e DOCKER_REPOSITORY=docker.io/komune/ -C c2-chaincode -e VERSION=$(VERSION)
+	make promote -e DOCKER_REPOSITORY=docker.io/komune/ -C c2-sandbox -e VERSION=$(VERSION)
 
 ## chaincode-api
 docker-chaincode-api-gateway-build:
 	VERSION=$(VERSION) ./gradlew build ${CHAINCODE_APP_PACKAGE} --imageName ${CHAINCODE_APP_IMG} -x test
 
 docker-chaincode-api-gateway-publish:
-	VERSION=$(VERSION) docker push ${CHAINCODE_APP_IMG}
+	@docker tag ${CHAINCODE_APP_IMG} ghcr.io/komune-io/${CHAINCODE_APP_IMG}
+	@docker push ghcr.io/komune-io/${CHAINCODE_APP_IMG}
+
+docker-chaincode-api-gateway-promote:
+	@docker tag ${CHAINCODE_APP_IMG} docker.io/komune/${CHAINCODE_APP_IMG}
+	@docker push docker.io/komune/${CHAINCODE_APP_IMG}
