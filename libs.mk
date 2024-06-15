a3739d7481f7dc3efed962952654a4da7d70624d
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-06-15 15:24:29 +0200

    test(libs.mk): add additional commands to start and log sandbox services for testing The additional commands added to the test-pre target in libs.mk file are necessary to start and log the c2-sandbox-ex02 service for testing purposes. This ensures that all required services are properly started and logged before running tests, improving the reliability and completeness of the testing process.

--- libs.mk
+++ libs.mk
@@ -12,7 +12,10 @@ build:
 test-pre:
 	@make dev up
 	@make dev c2-sandbox-ssm logs
+	@make dev c2-sandbox-ex02 logs
 	@make dev up
+	@make dev c2-sandbox-ssm logs
+	@make dev c2-sandbox-ex02 logs
 	sudo echo "127.0.0.1 ca.bc-coop.bclan" | sudo tee -a /etc/hosts
 	sudo echo "127.0.0.1 peer0.bc-coop.bclan" | sudo tee -a /etc/hosts
 	sudo echo "127.0.0.1 orderer.bclan" | sudo tee -a /etc/hosts
