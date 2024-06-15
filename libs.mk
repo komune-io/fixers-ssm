af39f350c045b0a5f26d063f701ffa5fc1815e27
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-06-15 15:35:26 +0200

    chore(libs.mk): add debug echo statements for better visibility during test preparation The changes were made to improve the debugging process during the test preparation phase by adding echo statements that provide clear separation and indication of each step. This enhances the visibility and understanding of the test execution flow.

--- libs.mk
+++ libs.mk
@@ -11,11 +11,21 @@ build:
 
 test-pre:
 	@make dev up
+	@echo "///////////////////////1"
+	@make dev c2-sandbox logs
+	@echo "///////////////////////2"
 	@make dev c2-sandbox-ssm logs
+	@echo "///////////////////////3"
 	@make dev c2-sandbox-ex02 logs
+	@echo "///////////////////////4"
 	@make dev up
+	@echo "///////////////////////5"
+	@make dev c2-sandbox logs
+	@echo "///////////////////////6"
 	@make dev c2-sandbox-ssm logs
+	@echo "///////////////////////7"
 	@make dev c2-sandbox-ex02 logs
+	@echo "///////////////////////8"
 	sudo echo "127.0.0.1 ca.bc-coop.bclan" | sudo tee -a /etc/hosts
 	sudo echo "127.0.0.1 peer0.bc-coop.bclan" | sudo tee -a /etc/hosts
 	sudo echo "127.0.0.1 orderer.bclan" | sudo tee -a /etc/hosts
