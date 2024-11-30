428273da000644361deecfa9ff60f042c7dc762d
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-11-30 16:39:19 +0100

    feat(build.gradle.kts): add mavenLocal repository for local dependency resolution
    test(chaincode-api-fabric): add unit tests for FabricChainCodeClient and FabricUserClient
    
    fix(ChaincodeRestEndpoint.kt): improve error message handling by removing redundant prefix
    
    refactor(SsmRequester.kt): introduce handleResponse method to streamline response handling and error management
    
    test(ssm-sdk-core): add SsmClientErrorTest to validate error handling in SSM client operations
    
    feat(ssm-sdk-dsl): introduce InvokeError and InvokeException classes for better error representation
    
    The addition of `mavenLocal` allows for resolving dependencies from the local Maven repository, facilitating local development and testing. New unit tests for `FabricChainCodeClient` and `FabricUserClient` ensure the functionality of chaincode interactions. Error message handling in `ChaincodeRestEndpoint.kt` is improved by removing redundant prefixes, making messages clearer. The `handleResponse` method in `SsmRequester.kt` centralizes response handling, improving code readability and error management. The `SsmClientErrorTest` validates error handling in SSM client operations, ensuring robustness. The introduction of `InvokeError` and `InvokeException` provides a structured way to represent and handle errors in the SDK.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 repositories {
 	mavenCentral()
 	maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots") }
+	mavenLocal()
 }
 
 dependencies {
---
+++ c2-ssm/ssm-sdk/ssm-sdk-dsl/src/commonMain/kotlin/ssm/sdk/dsl/InvokeException.kt
@@ -0,0 +1,3 @@
+package ssm.sdk.dsl
+
+class InvokeException(message: String, cause: Throwable?) : Exception(message, cause)
