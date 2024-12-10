379e649a951f299db1e30ba0db0b509cc186f55f
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-12-10 23:18:42 +0100

    refactor(HeraclesConfigProps.kt): change log level from info to debug for chaincode found message feat(WebFluxConfiguration.kt): add configuration for HTTP message codec memory size and debug logging The log level for the chaincode found message is changed to debug to reduce verbosity in production logs. In WebFluxConfiguration, the HTTP message codec's maximum in-memory size is set to 10 MB to handle larger payloads efficiently. Additionally, a CommandLineRunner bean is added to log the codec properties for debugging purposes, aiding in configuration verification during application startup.

--- c2-chaincode/chaincode-api/chaincode-api-gateway/src/main/kotlin/io/komune/c2/chaincode/api/gateway/config/WebFluxConfiguration.kt
+++ c2-chaincode/chaincode-api/chaincode-api-gateway/src/main/kotlin/io/komune/c2/chaincode/api/gateway/config/WebFluxConfiguration.kt
@@ -1,6 +1,10 @@
 package io.komune.c2.chaincode.api.gateway.config
 
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.codec.CodecProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.ResourceHandlerRegistry
@@ -23,5 +27,16 @@ class WebFluxConfiguration : WebFluxConfigurer {
         registry.addResourceHandler("/swagger-ui/**")
             .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
             .resourceChain(false)
+    }
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024) // Set to 10 MB
+    }
+
+    @Bean
+    fun debugCodecProperties(codecProperties: CodecProperties): CommandLineRunner {
+        return CommandLineRunner { args: Array<String?>? ->
+            println("Max In-Memory Size: " + codecProperties.maxInMemorySize)
+        }
     }
 }
