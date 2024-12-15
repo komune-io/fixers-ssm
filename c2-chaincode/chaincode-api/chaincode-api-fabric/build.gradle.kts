5742e2f346e4334ba1a8305dcd5776c132e9580c
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-12-15 19:44:11 +0100

    refactor(dependencies): centralize dependency management in Dependencies.kt
    Centralizes the management of dependencies by defining them in the
    Dependencies.kt file. This change improves maintainability and
    consistency across the project by reducing duplication and potential
    errors in dependency versions. It also simplifies the build.gradle.kts
    files by using the defined functions in Dependencies.kt to add
    dependencies, making the build scripts cleaner and easier to read.

--- c2-chaincode/chaincode-api/chaincode-api-fabric/build.gradle.kts
+++ c2-chaincode/chaincode-api/chaincode-api-fabric/build.gradle.kts
@@ -3,9 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation ("org.slf4j:slf4j-api:${Versions.slf4j}")
-    implementation ("org.hyperledger.fabric-sdk-java:fabric-sdk-java:${Versions.fabric}")
-    implementation( "com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
+
+    Dependencies.jackson(::implementation)
+    Dependencies.slf4j(::implementation)
+    Dependencies.fabricSdk(::api)
 
     Dependencies.test(::testImplementation)
 }
